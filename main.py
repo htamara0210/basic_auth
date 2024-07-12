f446e0926ef3175d454aa3bc4f6adb911dfc5625
Hernán Támara O <hernantama@outlook.es>
2024-07-12 17:51:50 -0500

    Se agrega estructura de jwt

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+from typing import Annotated
 from fastapi import FastAPI,Depends,HTTPException,status
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 import schemas
 from database import get_db
 import crud
 import security
 import models
+from jwt import ExpiredSignatureError
 
 
 app = FastAPI()
 
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def get_user_online(token: str = Depends(oauth2_scheme),session: Session = Depends(get_db)) -> models.User:
+    try:
+        payload = security.decode_token(token)
+        user_id = payload.get("sub")
+    except ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Token expirado")
+    return crud.get_user(db=session,user_id=user_id)
 
 @app.post("/create_user", response_model=schemas.User)
 def create_user(user: schemas.UserCreate,session: Session = Depends(get_db)):
     user.password = security.get_password_hash(user.password)
     return  crud.create_user(db=session,user=user)
 
-@app.get("/user/{user_id}", response_model=schemas.User)
-def saludo(user_id:int,session: Session = Depends(get_db)):
-    return crud.get_user(db=session,user_id=user_id)
+@app.get("/get_current_user", response_model=schemas.User)
+def get_current_user(user: models.User = Depends(get_user_online)):
 
-@app.post("/login", response_model=schemas.User)
+    return user
+
+@app.post("/login", response_model=security.Token)
 def login(user: schemas.UserBase,session: Session = Depends(get_db)):
 
     user_db = session.query(models.User).filter(models.User.email==user.email).first()
@@ -29,5 +43,28 @@ def login(user: schemas.UserBase,session: Session = Depends(get_db)):
 
     if not security.verify_password(user.password,user_db.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Contraseña o email incorrecta")
+    token = security.create_access_token(data={"sub":user_db.id},expires_delta=60)
 
-    return user_db
+    return  security.Token(access_token=token,token_type="bearer")
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],session: Session = Depends(get_db)
+) -> security.Token:
+    user_db = session.query(models.User).filter(models.User.username==form_data.username).first()
+    if not user_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Usuario con el username {form_data.username} no encontrado")
+
+    if not security.verify_password(form_data.password,user_db.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Contraseña o email incorrecta")
+
+    token = security.create_access_token(data={"sub":user_db.id},expires_delta=60)
+
+    return  security.Token(access_token=token,token_type="bearer")
+
+@app.patch("/update_email")
+def update_email(email: str, user: models.User = Depends(get_user_online), session: Session = Depends(get_db)):
+    user.email = email
+    session.commit()
+    session.refresh(user)
+    return user
--- security.py
+++ security.py
@@ -1,4 +1,12 @@
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
+import jwt
+from pydantic import BaseModel
+from typing import Union
+
+SECRET_KEY = "1d4e1791104e9b4418a26c7a5cd330f19ab1d953f33637f35d0f8d1373b64f26"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -8,3 +16,26 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Union[int, None] = None
+
+
+def create_access_token(data: dict, expires_delta: Union[int, None] = ACCESS_TOKEN_EXPIRE_MINUTES):
+    expires_delta = timedelta(minutes=expires_delta)
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
